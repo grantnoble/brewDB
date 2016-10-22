7c95a056c730ad9adb46a2047289c5d6445ff155
Grant Noble <grant.noble61@gmail.com>
2016-10-22 16:56:52 +1000

    fixed indentation

--- index.php
+++ index.php
@@ -12,12 +12,14 @@ header('Content-Type: text/html; charset="utf-8"', true);
 ?>
 
 <div class="container-fluid">
-<div class="jumbotron">
-      
-<h1>brewDB</h1>
-<p>Use the pre-loaded style and ingredient information to create your favourite beer recipes and record your brews.</p>
 
-</div>
+	<div class="jumbotron">
+      
+		<h1>brewDB</h1>
+		<p>Use the pre-loaded style and ingredient information to create your favourite beer recipes and record your brews.</p>
+
+	</div>
+	
 </div>
 
 <?php 
