c054a6d8ddd6dd5140e21e64a4c99233ed9fb4cf
Grant Noble <grant.noble61@gmail.com>
2016-10-22 15:57:07 +1000

    instead of hardcoded table names, query the database for the names and then drop them

--- all_tables_drop.php
+++ all_tables_drop.php
@@ -1,46 +1,41 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-    <title>All Tables Drop</title>
-</head>
-<body>
-
 <?php
 
 /* 
 all_tables_drop.php
 Drop (delete) all tables in the database
 */
+
+$page_title = 'Drop All Tables';
+include ('includes/header.html');
 header('Content-Type: text/html; charset="utf-8"', true);
-// connect to the database
-include('includes/database_connect.php');
 
-// array to hold the names of the tables to drop
-$array = ['fermentables', 'hops', 'yeasts', 'miscs', 'styles', 'persons', 'preferences', 'recipes', 'recipes_fermentables', 'recipes_hops', 'recipes_yeasts', 'recipes_miscs', 'recipes_persons', 'brews', 'brews_mashes', 'brews_ferments', 'brews_fermentables', 'brews_hops', 'brews_yeasts', 'brews_miscs', 'brews_persons'];
+echo '<div class="container">';
+echo '<h2>Drop All Tables</h2>';
 
-// for each table name in the array, drop the table
-foreach($array as $table)
+$query = "SHOW TABLES";
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection)); 
+while ($row = mysqli_fetch_array($result)) 
+{ 
+	$tables[] = "$row[0]";
+}
+
+foreach ($tables as $tablename)
 {
-	$query = 'DROP TABLE ' . $table;
+	$query = 'DROP TABLE ' . $tablename;
 	if (mysqli_query($connection, $query))
 	{
-		echo $table . ' table dropped.' . '<br />';
+		echo '<p>' . $tablename . ' table dropped.' . '</p>';
 	}
 	else
 	{
 	die(mysqli_error($connection));
 	}
 }
-
 mysqli_close($connection);
 
+echo '<form action="index.php">';
+echo '<input class="btn btn-default" type="submit" value="Back" />';
+echo '</form>';
 
+include ('includes/footer.html');
 ?>
-
-<form action="index.php">
-<br>
-<input type="submit" value="Back">
-</form>
-
-</body>
-</html> 
