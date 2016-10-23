5b1a743564107da097e7e7c0dabc31a71f997a32
Grant Noble <grant.noble61@gmail.com>
2016-10-23 17:52:42 +1000

    tidy up and make consistent

--- brewdb_backup.php
+++ brewdb_backup.php
@@ -1,28 +1,29 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-    <title>Brewdb Backup</title>
-</head>
-<body>
-
 <?php
 
 /* 
 brewdb_backup.php
 Backup the brewdb database
 */
+
+$page_title = 'Backup Database';
+include ('includes/header.html');
 header('Content-Type: text/html; charset="utf-8"', true);
-// connect to the database
-include('includes/database_connect.php');
+
+echo '<div class="container">';
+echo '<h2>Backup Database</h2>';
 
 $backup_file = $database . "_backup_" . date("Y-m-d-H-i-s") . ".sql";
 $command = "mysqldump -u " . $username . " -p" . $password . " brewdb_dev --skip-extended-insert --result-file=" . $backup_file;
 
 system($command);
 
-echo "Backup to " . $backup_file . " successful. Click 'back' to continue";
+echo '<p>Backup to ' . $backup_file . ' successful.';
 
+mysqli_close($connection);
+
+echo '<form action="index.php">';
+echo '<input class="btn btn-default" type="submit" value="Back" />';
+echo '</form>';
+
+include ('includes/footer.html');
 ?> 
-
-</body>
-</html> 
