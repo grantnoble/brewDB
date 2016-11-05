b66f1d65cd841cff7aa791d2bdecccba45c83510
Grant Noble <grant.noble61@gmail.com>
2016-11-05 10:01:49 +1000

    display the fermentable details on the form

--- js_files/getrecipeinfo.js
+++ js_files/getrecipeinfo.js
@@ -15,6 +15,7 @@ xmlhttp.onreadystatechange=function()
 {
 	if (xmlhttp.readyState==4 && xmlhttp.status==200)
 	{
+		// display the basic recipe and style details
 		document.forms[0].type.value=xmlhttp.responseXML.getElementsByTagName("recipe_type")[0].childNodes[0].nodeValue;
 		document.forms[0].style.value=xmlhttp.responseXML.getElementsByTagName("style_name")[0].childNodes[0].nodeValue;
 		document.forms[0].style_og_min.value=xmlhttp.responseXML.getElementsByTagName("style_og_min")[0].childNodes[0].nodeValue;
@@ -32,6 +33,33 @@ xmlhttp.onreadystatechange=function()
 		document.forms[0].est_abv.value=xmlhttp.responseXML.getElementsByTagName("recipe_est_abv")[0].childNodes[0].nodeValue;
 		document.forms[0].est_ibu.value=xmlhttp.responseXML.getElementsByTagName("recipe_est_ibu")[0].childNodes[0].nodeValue;
 		document.forms[0].est_color.value=xmlhttp.responseXML.getElementsByTagName("recipe_est_color")[0].childNodes[0].nodeValue;
+		
+		var i;
+		// for each of the 15 possible fermentables, if there is one available, display it. Else set the rest of the fields to null
+		for (i = 0; i < 15; i++)
+		{
+			if (typeof xmlhttp.responseXML.getElementsByTagName("fermentable_name")[i] !== 'undefined')
+			{
+				document.forms[0]["fermentable"+i+"_name"].value=xmlhttp.responseXML.getElementsByTagName("fermentable_name")[i].childNodes[0].nodeValue;
+				document.forms[0]["fermentable"+i+"_amount"].value=xmlhttp.responseXML.getElementsByTagName("fermentable_amount")[i].childNodes[0].nodeValue;
+				document.forms[0]["fermentable"+i+"_type"].value=xmlhttp.responseXML.getElementsByTagName("fermentable_type")[i].childNodes[0].nodeValue;
+				document.forms[0]["fermentable"+i+"_yield"].value=xmlhttp.responseXML.getElementsByTagName("fermentable_yield")[i].childNodes[0].nodeValue;
+				document.forms[0]["fermentable"+i+"_color"].value=xmlhttp.responseXML.getElementsByTagName("fermentable_color")[i].childNodes[0].nodeValue;
+				document.forms[0]["fermentable"+i+"_use"].value=xmlhttp.responseXML.getElementsByTagName("fermentable_use")[i].childNodes[0].nodeValue;
+			}
+			else
+			{
+				document.forms[0]["fermentable"+i+"_name"].value=null;
+				document.forms[0]["fermentable"+i+"_amount"].value=null;
+				document.forms[0]["fermentable"+i+"_percent"].value=null;
+				document.forms[0]["fermentable"+i+"_type"].value=null;
+				document.forms[0]["fermentable"+i+"_yield"].value=null;
+				document.forms[0]["fermentable"+i+"_color"].value=null;
+				document.forms[0]["fermentable"+i+"_use"].value=null;
+			}
+		}
+		// finally, calculate the fermentable percentages
+		calc_percent();
 	}
 }
 
