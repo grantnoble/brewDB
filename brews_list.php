132dcab659b65c0f4040984aee18773c381d3aa1
Grant Noble <grant.noble61@gmail.com>
2016-11-09 22:44:45 +1000

    fix up the brews_list

--- brews_list.php
+++ brews_list.php
@@ -13,47 +13,57 @@ header('Content-Type: text/html; charset="utf-8"', true);
 $sortby = "date";
 
 // check if the 'id' variable is set in URL, and check that it is valid
-if (isset($_GET['id']) && ($_GET['id']=='date' || $_GET['id']=='style'))
+if (isset($_GET['id']) && ($_GET['id']=='date' || $_GET['id']=='style_category' || $_GET['id']=='style_name'))
 {
 	$sortby = $_GET['id'];
 }
 
 // set the query statement, either by style or by date
-if ($sortby=='style')
+if ($sortby=='style_category')
 {
-	$query = "SELECT brew_id, brew_name, brew_type, brew_date, style_category_number, style_style_letter, style_name FROM brews INNER JOIN styles ON brew_style_id=style_id ORDER BY style_category_number, style_style_letter, brew_date";
+	$query = "SELECT brew_id, brew_name, brew_type, brew_date, style_style_guide, style_category_number, style_subcategory, style_name FROM brews INNER JOIN styles ON brew_style_id=style_id ORDER BY style_category_number, style_subcategory, brew_date";
+}
+elseif ($sortby=='style_name')
+{
+	$query = "SELECT brew_id, brew_name, brew_type, brew_date, style_style_guide, style_category_number, style_subcategory, style_name FROM brews INNER JOIN styles ON brew_style_id=style_id ORDER BY style_name, brew_date";
 }
 else
 {
-	$query = "SELECT brew_id, brew_name, brew_type, brew_date, style_category_number, style_style_letter, style_name FROM brews INNER JOIN styles ON brew_style_id=style_id ORDER BY brew_date DESC";
+	$query = "SELECT brew_id, brew_name, brew_type, brew_date, style_style_guide, style_category_number, style_subcategory, style_name FROM brews INNER JOIN styles ON brew_style_id=style_id ORDER BY brew_date DESC";
 }
 
 // get results from database
-$brews = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$recipes = mysqli_query($connection, $query) or die(mysqli_error($connection));
 
 // display data in table
-echo '<tr><td><h2>List Brews</h2></td></tr>';
-echo '<table class="list_table">';
-echo '<tr> <th>Name</th> <th colspan="2"><a href="brews_list.php?id=style">Style and Category</a></th> <th>Type</th> <th><a href="brews_list.php?id=date">Date</a></th> <th>Action</th> </tr>';
+echo '<div class="container">';
+echo '<h2>List Brews</h2>';
+echo '<div class="table-responsive">';
+echo '<table class="table table-hover table-condensed">';
+echo '<tr class="info"> <th>Brew Name</th><th><a href="brews_list.php?id=style_category">BJCP Category</a></th><th><a href="brews_list.php?id=style_name">Style Name</a></th><th>Brew Type</th> <th><a href="brews_list.php?id=date">Date</a></th></tr>';
 
 // loop through results of database query, displaying them in the table
-while($row = mysqli_fetch_array( $brews ))
+while($row = mysqli_fetch_array( $recipes ))
 {
     // echo out the contents of each row into a table
     echo '<tr>';
-    echo '<td><a href="brews_view.php?id=' . $row['brew_id'] . '">' . $row['brew_name'] . '</a></td>';
-    echo '<td>' . $row['style_category_number'] . $row['style_style_letter'] . '</td>';
+    echo '<td><div class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">' . $row['brew_name'] . '</a>';
+	echo '<ul class="dropdown-menu">';
+	echo '<li><a href="brew_view.php?id=' . $row['brew_id'] . '" role="button">View</a></li>';
+	echo '<li><a href="brew_edit.php?id=' . $row['brew_id'] . '" role="button">Edit</a></li>';
+	echo '<li><a href="brew_delete.php?id=' . $row['brew_id'] . '" role="button">Delete</a></li>';
+    echo '</ul></div></td>';
+    echo '<td>' . $row['style_style_guide']. ' ' . $row['style_category_number'] . $row['style_subcategory'] . '</td>';
     echo '<td>' . $row['style_name'] . '</td>';
     echo '<td>' . $row['brew_type'] . '</td>';
 	echo '<td>' . $row['brew_date'] . '</td>';
-    echo '<td><a href="brews_view.php?id=' . $row['brew_id'] . '">View</a> / ';
-    echo '<a href="brews_edit.php?id=' . $row['brew_id'] . '">Edit</a> / ';
-    echo '<a href="brews_delete.php?id=' . $row['brew_id'] . '">Delete</a></td>';
     echo "</tr>";
     }
 
 // close table
 echo "</table>";
+echo '</div>';
+echo '</div>';
 
 ?>
 
